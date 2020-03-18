f33d27227e504f2e6b66743c3117fe9be340fde2
EthanYooBuzzvil <ethan.yoo@buzzvil.com>
2020-03-18 14:35:24 +0900

    comment

--- feature/feed/src/main/java/com/dynatic/feed/Feed.kt
+++ feature/feed/src/main/java/com/dynatic/feed/Feed.kt
@@ -20,6 +20,7 @@ class Feed @Inject constructor(
     }
 
     fun inflateView(parentContext: Context): View {
+        // TODO Component Architecture 적용하는 것이 더 좋은 구조를 가져갈 수 있을 것 같음
         val feedView = FeedView(parentContext)
         feedComponent!!.inject(feedView) // This must be called after Feed.init()
         return feedView
